ad3d920d8fdbeb6114871160cd89011171b61218
yesukumar bhuvanagiri <kumaryesu2000@gmail.com>
2025-05-17 16:47:37 +0530

    feat: Initialize React app with essential files and configurations

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+         Branch
+        </p>
+        <h1>header one</h1>
+        <h1>Initail change in app.js</h1>    
+        <h1>Edited by Yesu</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
